553cb1fc64628941012325e24e62a091bd5a7c51
Barsik1976 <shlippenbah@mail.ru>
2021-03-10 11:24:55 +0300

    Добавление префикса

--- js/popup.js
+++ js/popup.js
@@ -33,7 +33,18 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("modal-error");
 });
 
-
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !mail.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.getItem("login", login.value);
+    }
+  }
+});
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
